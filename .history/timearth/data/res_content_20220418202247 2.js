e2148dcdc3f0200c1f56b27b15dc09bc609c792b
cho196 <cho196@my.bcit.ca>
2022-05-20 08:11:53 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ .history/timearth/data/res_content_20220418202247 2.js
@@ -0,0 +1,29 @@
+export const goodResults = [
+    {
+        title: "You often opted for plant-based meals instead of meat!",
+        desc: " A global shift to a plant-based diet could reduce greenhouse gases caused by food production by 70% within 30 years."
+    },
+    {
+        title: "You frequently used sustainable transportation!",
+    },
+    {
+        title: "You kept your water usage low through your shower frequency!",
+    },
+    {
+        title: "You kept your electricity usage to a minimum!",
+    },
+    {
+        title: "You produced a very small amount of waste!",
+    },
+    {
+        title: "Your shopping habits were sustainable!",
+    },
+
+]
+
+export const badResults = [
+    {},
+    {},
+    {},
+    {},
+]
